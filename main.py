befbd0cf566310a3e721c05307ad546226eb711a
Gavin18049 <lawsongavin928@gmail.com>
2024-08-05 22:49:18 -0400

    push csv files

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import csv
+csvpath = os.path.join('analysis', 'budget_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    for row in csvreader:
+        print(row)
+          
